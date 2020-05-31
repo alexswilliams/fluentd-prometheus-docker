3c78e8f6cf07b458af1a57ac73984a5e11272c68
Alex Williams <alex.williams@skybettingandgaming.com>
2020-05-31 01:37:59 +0100

    Fluentd 1.10.4 with prom exporter 1.8.0

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,23 @@
-FROM fluent/fluentd:v1.8.1-debian-1.0
+ARG FLUENT_VERSION
+FROM fluent/fluentd:${FLUENT_VERSION}
 
 LABEL org.label-schema.build-date=$BUILD_DATE \
       org.label-schema.name="FluentD with Prometheus Plugin" \
       org.label-schema.description="FluentD with the prometheus plugin pre-bundled" \
       org.label-schema.vcs-ref=$VCS_REF \
-      org.label-schema.vcs-url="https://github.com/alexswilliams/home-cluster" \
+      org.label-schema.vcs-url="https://github.com/alexswilliams/fluentd-prometheus-docker" \
       org.label-schema.schema-version="1.0"
 
 
 # Use root account to use apt
 USER root
 
+ARG PROM_PLUGIN_VERSION
+# New versions from: https://github.com/fluent/fluent-plugin-prometheus/releases
 RUN buildDeps="sudo make gcc g++ libc-dev ruby-dev" \
     && apt-get update \
     && apt-get install -y --no-install-recommends $buildDeps \
-    && sudo gem install fluent-plugin-prometheus --version="~>1.6.0" \
+    && sudo gem install fluent-plugin-prometheus --version="${PROM_PLUGIN_VERSION}" \
     && sudo gem sources --clear-all \
     && SUDO_FORCE_REMOVE=yes \
     apt-get purge -y --auto-remove \
--- build.sh
+++ build.sh
@@ -1,12 +1,31 @@
 #!/usr/bin/env bash
 
+# If this script fails to run on the `docker buildx` line, try:
+# - enabling Experimental Features within docker
+# - running `docker buildx create` followed by `docker buildx use ...` to create a builder instance
+
 set -ex
 
-tag="$(grep 'FROM.*' Dockerfile | sed 's/FROM //' | sed 's#.*/##' | sed 's/:/-/')--$(date +%Y%m%d-%H%M)"
+function buildAndPush {
+    local fluentVersion=$1
+    local promPluginVersion=$2
+    local imagename="alexswilliams/fluentd-with-prometheus-plugin"
+    local latest="last-build"
+    if [ "$3" == "latest" ]; then latest="latest"; fi
 
-docker build \
-    -t london.alexswilliams.co.uk:5000/fluentd-custom:$tag \
-    --build-arg BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ") \
-    --build-arg VCS_REF=$(git rev-parse --short HEAD) \
-    .
-docker push london.alexswilliams.co.uk:5000/fluentd-custom:$tag
+    docker buildx build \
+        --platform=linux/amd64 \
+        --build-arg FLUENT_VERSION=${fluentVersion} \
+        --build-arg PROM_PLUGIN_VERSION=${promPluginVersion} \
+        --build-arg BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ") \
+        --build-arg VCS_REF=$(git rev-parse --short HEAD) \
+        --tag ${imagename}:${fluentVersion} \
+        --tag ${imagename}:${fluentVersion}-${promPluginVersion} \
+        --tag ${imagename}:${latest} \
+        --push \
+        --file Dockerfile .
+}
+
+buildAndPush "v1.10.4-debian-1.0" "1.8.0" "latest"
+
+curl -X POST "https://hooks.microbadger.com/images/alexswilliams/fluentd-with-prometheus-plugin/VZtnrWrOcA9tdGmL7na57qDh2JU="
