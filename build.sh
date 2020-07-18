639d23f727a999d15b28dbca291fc34f41b24fdd
Alex Williams <alex.williams@skybettingandgaming.com>
2020-07-18 04:19:32 +0100

    Version bump to 1.11.1

--- build.sh
+++ build.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-# If this script fails to run on the `docker buildx` line, try:
-# - enabling Experimental Features within docker
-# - running `docker buildx create` followed by `docker buildx use ...` to create a builder instance
-
 set -ex
 
 function buildAndPush {
@@ -13,7 +9,7 @@ function buildAndPush {
     local latest="last-build"
     if [ "$3" == "latest" ]; then latest="latest"; fi
 
-    docker buildx build \
+    DOCKER_BUILDKIT=1 docker build \
         --platform=linux/amd64 \
         --build-arg FLUENT_VERSION=${fluentVersion} \
         --build-arg PROM_PLUGIN_VERSION=${promPluginVersion} \
@@ -22,10 +18,14 @@ function buildAndPush {
         --tag ${imagename}:${fluentVersion} \
         --tag ${imagename}:${fluentVersion}-${promPluginVersion} \
         --tag ${imagename}:${latest} \
-        --push \
         --file Dockerfile .
+
+    DOCKER_BUILDKIT=1 docker push ${imagename}:${fluentVersion}
+    DOCKER_BUILDKIT=1 docker push ${imagename}:${fluentVersion}-${promPluginVersion}
+    DOCKER_BUILDKIT=1 docker push ${imagename}:${latest}
 }
 
-buildAndPush "v1.10.4-debian-1.0" "1.8.0" "latest"
+# buildAndPush "v1.10.4-debian-1.0" "1.8.0"
+buildAndPush "v1.11.1-debian-1.0" "1.8.2" latest
 
 curl -X POST "https://hooks.microbadger.com/images/alexswilliams/fluentd-with-prometheus-plugin/VZtnrWrOcA9tdGmL7na57qDh2JU="
